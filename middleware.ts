8f0bc6292aebe9a3c499894a87afbf7f8bb4a7a6
zce <w@zce.me>
2024-01-13 11:50:09 +0800

    chore: meta tweaks

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,9 @@
+import { NextResponse } from 'next/server'
 import type { NextMiddleware } from 'next/server'
 
 export const middleware: NextMiddleware = async request => {
-  return Response.json(process.env)
+  if (request.nextUrl.pathname.startsWith('/mw-env')) {
+    return Response.json(process.env)
+  }
+  return NextResponse.next()
 }
